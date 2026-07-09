f15ec7ab763f5dbba95e4f2b70937f863b30155f
udaychittala-niruthi <uday.kumar@niruthi.com>
2026-07-09 12:18:07 +0530

    feat: set sitemap to force-dynamic to ensure fresh content retrieval

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next"
 import { prisma } from "@/lib/db/prisma"
 
+export const dynamic = "force-dynamic"
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await prisma.post.findMany({
     where: { published: true },
